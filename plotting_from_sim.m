f7a173cbe31aae21a7b92adca428ca1212ae60b9
c-radicallis <afonso.henrique@live.com.pt>
2025-01-15 15:44:32 +0000

    commit

--- plotting_from_sim.m
+++ plotting_from_sim.m
@@ -1,54 +1,60 @@
 clear all
 close all
 
-s=tf('s');
-tf_G= zpk(-1 , [-4 , -2 , 1] , 10 );
-
+s = tf('s');
+tf_G = zpk(-1, [-4, -2, 1], 10);
 
 A_1 = 0.5; % delta
-S_1=A_1;
+S_1 = A_1;
 k = 2;
 
 A_2 = 1; % delta
-S_2=A_2;
+S_2 = A_2;
 M_2 = 1;
 
-A_3=2;
-h=A_3;
-B_3=0.5;
-M_3=B_3;
-
+A_3 = 2;
+h = A_3;
+B_3 = 0.5;
+M_3 = B_3;
 
 ic = 0;
-Freq = logspace(0, 1.5, 4); % Frequency range (1 Hz to 100 Hz logarithmically spaced)
-Amps = [0.5,1,2];       % Different amplitude values to test
-t_end=10;
+Freq = logspace(-0.5, 1.2, 4); % Frequency range (1 Hz to ~30 Hz logarithmically spaced)
+Amps = [2,4];         % Different amplitude values to test
+
+% Specify the folder to save plots
+save_folder = 'Sim_Results'; % Change this to your desired folder
+if ~exist(save_folder, 'dir')
+    mkdir(save_folder); % Create the folder if it doesn't exist
+end
+
 
 % Loop through each combination of Amp and Freq
 for i = 1:length(Amps)
     for j = 1:length(Freq)
         Amp = Amps(i);
         freq = Freq(j);
-        
-        % % Generate sinusoidal input signal
-        % t = 0:0.001:t_end; % Time vector (5 seconds, 1 ms resolution)
-        % u = Amp * sin( freq * t); % Sinusoidal input
-        
-        % % Assign input to Simulink model workspace
-        % assignin('base', 'u', u);
-        % assignin('base', 't', t);
-        
+        t_end = 2*2*pi/freq;
+
         % Run Simulink model
-        out = sim("problem1_model.slx");
+        assignin('base', 'StopTime', t_end);
+        out = sim("problem1_model.slx",'StopTime', num2str(t_end));
         
         % Plot simulation results
         figure;
         hold on;
-        plot(out.simout)%, 'DisplayName', sprintf('Freq=%.1f Hz, Amp=%.1f', freq, Amp));
-        hold off;
+        for k = 1:4
+            plot(out.simout.Time, out.simout.Data(:,k), 'LineWidth', 1.5);
+            end
         xlabel('Time (s)');
         ylabel('Output');
-        title(sprintf('System Response for u = %.1f sin(%.1f t)', Amps(i),Freq(i)));
-        legend('show');
+        title(sprintf('System Response: u = %.1f sin( %.2f t)', Amp, freq));  
+        legend('Input','Dead zone',' Saturation w/ dead zone', 'Hysteresis', 'Location', 'best');
+        axis([0 t_end -(Amps(i)+2) Amps(i)+2])
+        hold off;
+
+        % Construct file path and save plot as PNG
+        filename = sprintf('Amp%.1f_Freq%.2f.png', Amp, freq);
+        filepath = fullfile(save_folder, filename);
+        saveas(gcf, filepath);
     end
 end
